c7b33a6f6ecdc527e3a2912ea079413a2bc77819
Tran Le Quang Trieu <trieutran025@gmail.com>
2022-12-22 19:51:47 +0700

    task3

---
+++ Task3/MenuDrink/moneyDrink.js
@@ -0,0 +1,25 @@
+function OrderDrink1() {
+    let input =document.getElementById("input1").value;
+    document.getElementById('read1').innerText="Giá của sản phẩm là :"+input *10000;
+}
+function OrderDrink2() {
+    let input =document.getElementById("input2").value;
+    document.getElementById('read2').innerText="Giá của sản phẩm là :"+input *10000;
+
+}
+function OrderDrink3() {
+    let input =document.getElementById("input3").value;
+    document.getElementById('read3').innerText="Giá của sản phẩm là :"+input *10000;
+}
+function OrderDrink4() {
+    let input =document.getElementById("input4").value;
+    document.getElementById('read4').innerText="Giá của sản phẩm là :"+input *10000;
+}
+function OrderDrink5() {
+    let input =document.getElementById("input5").value;
+    document.getElementById('read5').innerText="Giá của sản phẩm là :"+input *10000;
+}
+function OrderDrink6() {
+    let input =document.getElementById("input6").value;
+    document.getElementById('read6').innerText="Giá của sản phẩm là :"+input *10000;
+}
